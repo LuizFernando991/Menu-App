e9a6c1d329a78c859403534082c3d6e2a4a420e1
unknown <lfernando.r991@gmail.com>
2024-02-02 10:57:14 -0300

    SETUP: typeorm setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [__dirname + '/**/*.entity(.ts,.js)'],
+      synchronize: true
+    })
+  ],
   controllers: [],
   providers: []
 })
