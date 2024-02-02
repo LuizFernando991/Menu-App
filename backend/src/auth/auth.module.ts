a8d91153893ec49cec01ae0fc374faabb0faff82
unknown <lfernando.r991@gmail.com>
2024-02-02 13:15:13 -0300

    CHORE: config jwtmodule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,9 +3,15 @@ import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthEntity } from './auth.entity'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AuthEntity])],
+  imports: [
+    TypeOrmModule.forFeature([AuthEntity]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secret'
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
