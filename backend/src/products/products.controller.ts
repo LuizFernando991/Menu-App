6bec322d16bd762e4c5e9be3bf98c9a48acaf868
unknown <lfernando.r991@gmail.com>
2024-02-06 17:38:04 -0300

    Chore: create a product route

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors
+} from '@nestjs/common'
+import { ProductsService } from './products.service'
+import { FileInterceptor } from '@nestjs/platform-express'
+import { CreateProductDto } from './product.dto'
 
-@Controller('products')
-export class ProductsController {}
+@Controller('product')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('photo'))
+  async create(
+    @Body() createProductDto: CreateProductDto,
+    @UploadedFile() photo
+  ): Promise<any> {
+    if (!photo) {
+      throw new BadRequestException('photo is required')
+    }
+    const imagePath = photo.filename
+    return this.productsService.create(createProductDto, imagePath)
+  }
+}
--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { ProductEntity } from './product.entity'
+import { Repository } from 'typeorm'
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private readonly productRepository: Repository<ProductEntity>
+  ) {}
+
+  async create(productData, imagePath: string): Promise<any> {
+    const product = this.productRepository.create({
+      ...productData,
+      photo: imagePath
+    })
+    return await this.productRepository.save(product)
+  }
+}
