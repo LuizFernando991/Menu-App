9cae784872a528893bb8c57b0d79d909494d6328
unknown <lfernando.r991@gmail.com>
2024-02-06 15:12:22 -0300

    Test: add tests to categories controller

--- backend/src/categories/categories.controller.spec.ts
+++ backend/src/categories/categories.controller.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { CategoriesController } from './categories.controller'
+import { CategoriesService } from './categories.service'
+import { CategoryEntity } from './category.entity'
+
+jest.mock('./categories.service')
 
 describe('CategoriesController', () => {
   let controller: CategoriesController
+  let service: CategoriesService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [CategoriesController]
+      controllers: [CategoriesController],
+      providers: [CategoriesService]
     }).compile()
 
     controller = module.get<CategoriesController>(CategoriesController)
+    service = module.get<CategoriesService>(CategoriesService)
   })
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined()
+  describe('findAll', () => {
+    it('should return an array of categories', async () => {
+      const mockCategories: CategoryEntity[] = [
+        { id: 1, name: 'Category 1', parent: null, children: null },
+        { id: 2, name: 'Category 2', parent: null, children: null }
+      ]
+
+      jest.spyOn(service, 'findAll').mockResolvedValue(mockCategories)
+
+      const result = await controller.findAll()
+
+      expect(result).toEqual(mockCategories)
+    })
+
+    it('should call categoriesService.findAll', async () => {
+      const spy = jest.spyOn(service, 'findAll')
+
+      await controller.findAll()
+
+      expect(spy).toHaveBeenCalled()
+    })
   })
 })
