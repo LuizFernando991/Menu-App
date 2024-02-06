53e646ca1feb7cfa52ffe33af02245293b3023da
unknown <lfernando.r991@gmail.com>
2024-02-06 17:37:48 -0300

    Chore: add multer config

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -1,11 +1,35 @@
-import { Module } from '@nestjs/common'
+import { BadRequestException, Module } from '@nestjs/common'
 import { ProductsController } from './products.controller'
 import { ProductsService } from './products.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ProductEntity } from './product.entity'
+import { MulterModule } from '@nestjs/platform-express'
+import { diskStorage } from 'multer'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ProductEntity]),
+    MulterModule.register({
+      dest: './uploads',
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (_, file, callback) => {
+          const filename = `${Date.now()}-${file.originalname}`
+          callback(null, filename)
+        }
+      }),
+      fileFilter: (req, file, callback) => {
+        const mimetype = file.mimetype
+        if (!['image/jpeg', 'image/jpg'].includes(mimetype)) {
+          return callback(
+            new BadRequestException('only jpg or jpeg files are allowed'),
+            false
+          )
+        }
+        callback(null, true)
+      }
+    })
+  ],
   controllers: [ProductsController],
   providers: [ProductsService]
 })
