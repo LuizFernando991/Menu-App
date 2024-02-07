0ee66c12f79198103246b23e73d14e698ac50809
unknown <lfernando.r991@gmail.com>
2024-02-07 11:24:06 -0300

    Feat: add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.scss'
 
+// pages
+import Login from './pages/Login'
+
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Oi</div>}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </Router>
   )
--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,11 +1,43 @@
 import { FC } from 'react'
+import { useForm } from 'react-hook-form'
+import Input from '../../components/Input'
 import './login.styles.scss'
 
 const Login: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
+
+  const onSubmit = (data: { email: string; password: string }) => {
+    console.log(data)
+  }
   return (
     <div className="login-container">
       <main className="login-form">
         <h1>Informe o seu e-mail e senha para continuar</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            register={register}
+            name="email"
+            placeholder="email"
+            error={errors['email'] ? errors['email'].message : ''}
+          />
+          <Input
+            register={register}
+            name="password"
+            placeholder="password"
+            type="password"
+            error={errors['password'] ? errors['password'].message : ''}
+          />
+          <button>Entrar</button>
+        </form>
       </main>
     </div>
   )
