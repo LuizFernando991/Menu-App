af3f098450f54640af6a14682189358f263413bf
unknown <lfernando.r991@gmail.com>
2024-02-07 10:44:20 -0300

    Setup: global styles

---
+++ frontend/src/pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import './login.styles.scss'
+
+const Login: FC = () => {
+  return (
+    <div className="login-container">
+      <main className="login-form">
+        <h1>Informe o seu e-mail e senha para continuar</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Login
