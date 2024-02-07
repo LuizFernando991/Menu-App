b66bbcfd2af52efe9da191d5cee2fd8ff43671cc
unknown <lfernando.r991@gmail.com>
2024-02-07 11:59:17 -0300

    Chore: implements authcontext provider and toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.scss'
-
+// contexts
+import { AuthProvider } from './context/AuthContext'
 // pages
 import Login from './pages/Login'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
+      <Toaster containerStyle={{ fontSize: '1.6rem' }} />
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   )
 }
