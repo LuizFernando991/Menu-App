71d3c8460d90f0e098852a4ed20e3e2c20971503
unknown <lfernando.r991@gmail.com>
2024-02-08 13:27:46 -0300

    Chore: add product creation page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import './App.scss'
 // contexts
 import { AuthProvider } from './context/AuthContext'
 // pages
 import Login from './pages/Login'
-import { Toaster } from 'react-hot-toast'
 import NavBar from './components/NavBar'
+import CreateProduct from './pages/CreateProduct'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <NavBar />
           <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/create" element={<CreateProduct />} />
           </Routes>
         </div>
       </AuthProvider>
---
+++ frontend/src/pages/CreateProduct/index.tsx
@@ -0,0 +1,51 @@
+import { FC } from 'react'
+import ProductForm from '../../components/ProductForm'
+import { useApi } from '../../hooks/useApi'
+import './createproduct.styles.scss'
+import toast from 'react-hot-toast'
+import { ProductFormType } from '../../types/Prodcut'
+import { useNavigate } from 'react-router-dom'
+
+const CreateProduct: FC = () => {
+  const api = useApi()
+  const navigate = useNavigate()
+  const onSubmit = async (data: ProductFormType) => {
+    if (!data.categoryIds?.length) {
+      toast.error('Selecione ao menos uma categoria para seu item!')
+      return
+    }
+    if (!data.photo) {
+      toast.error('Selecione uma foto para seu produto!')
+      return
+    }
+    const formData = new FormData()
+    Object.entries(data).forEach(([key, value]) => {
+      if (value) {
+        if (key === 'photo' && value instanceof File) {
+          formData.append(key, value)
+        } else {
+          formData.append(key, JSON.stringify(value))
+        }
+      }
+    })
+    try {
+      await api.post('/product', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+      toast.success('Item criado!')
+      navigate('/')
+    } catch (err) {
+      toast.error('Algo deu errado, tente mais tarde!')
+    }
+  }
+  return (
+    <main className="create-product-container">
+      <h1>Adicione um novo item ao seu cardápio!</h1>
+      <ProductForm onSubmit={onSubmit} />
+    </main>
+  )
+}
+
+export default CreateProduct
