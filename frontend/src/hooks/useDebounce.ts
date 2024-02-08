1eed88e3c1b0d01e55976f3fd42131f96c1c269e
unknown <lfernando.r991@gmail.com>
2024-02-08 17:52:50 -0300

    Chore: add useDebounce

---
+++ frontend/src/hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+
+export const useDebounce = (value: string | number, delay: number) => {
+  const [debounceValue, setDebounceValue] = useState(value)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebounceValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [value, delay])
+
+  return debounceValue
+}
